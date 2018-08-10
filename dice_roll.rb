a413974e60a1e644c5735c4aacbd0e38a2007b93
Samthedriver <amanuelabraham0202@gmail.com>
2018-08-10 18:56:56 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -5,7 +5,7 @@
 def roll
   # code goes here
   myArray = ['1', '2', '3', '4', '5', '6']
-  rand(myArray.length)
+  myArray[rand(myArray.length)]
   #myArray[rand(myArray.length)]
   #rand(1..6)
 end
