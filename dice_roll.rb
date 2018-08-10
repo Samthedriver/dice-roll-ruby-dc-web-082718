4e51b06351aa2f857df326e4c169030388fe5dfe
Samthedriver <amanuelabraham0202@gmail.com>
2018-08-10 19:01:44 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -6,7 +6,7 @@ def roll
   # code goes here
   myArray = ['1', '2', '3', '4', '5', '6']
   puts myArray
-  puts myArray[random(myArray.length)]
+  puts myArray[rand(myArray.length)]
   integer(myArray[rand(myArray.length)])
   #myArray[rand(myArray.length)]
   #rand(1..6)
